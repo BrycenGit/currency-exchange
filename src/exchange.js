26f6625e299ebc89fbf8545c65c660914b19fb43
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-11 11:25:10 -0700

    erase everything i did and go back to what i had working

--- src/exchange.js
+++ src/exchange.js
@@ -1,17 +1,21 @@
 export default class CurrencyExchange {
   static getCurrency(baseCurrency) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response)
-        } else {
-          reject(request.response)
-        }
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`)
+    .then(function(response) {
+      if (!response.ok) {
+        console.log(response)
+        throw Error(response.statusText);
       }
-      request.open('get', url, true);
-      request.send();
+      return response.json();
+    })
+    .catch(function(error) {
+      console.log(error);
+      return error;
     })
   }
+
+  // converter(baseCurrencyAmount, convertTo) {
+  //   baseCurrencyAmount * response.
+  // }
+
 }
