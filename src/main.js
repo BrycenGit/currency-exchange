893bfc4a63be99e85c0c102ad9ac1c0ea4f85cc1
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-11 16:00:16 -0700

    add list of currency function

--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ function showMath(amountToExchange, response, toExchange) {
   $('.show-answer').text(`your USD is worth ${amountToExchange * response.conversion_rates[toExchange]} ${toExchange}`)
 }
 
+function displayCurrencies(array) {
+  let html = ('');
+  html += `<p>Here is a list of supported currencies:</p>`
+  html += `<ul>`
+  for (let i = 0; i < array.length; i++) {
+    html += `<li>${array[i]}</li>`
+  }
+  html += `</ul>`
+  $('.currency-list').html(html);
+}
+
 $(document).ready(function() {
   $('#exchange-me').click(function() {
     let amountToExchange = $('#dollars').val();
@@ -18,7 +29,9 @@ $(document).ready(function() {
           $('.show-answer').text(response)
         } else if (!response.conversion_rates[currencyToExchange]) {
           $('.show-answer').html(`Error! Please refer to our list of suppported currencies: <a href='https://www.exchangerate-api.com/docs/supported-currencies'>Currencies</a>`)
-          let object = response.conversion_rates;
+          let object = response.conversion_rates;0
+          let array = Object.keys(object)
+          displayCurrencies(array);
           console.log(Object.keys(object))
         } else {
         showMath(amountToExchange, response, currencyToExchange);
