96c437c5ffd04fc1e639da651e51ea94fe15e402
Brycen Bartolome <brycenbartolome@gmail.com>
2020-09-11 09:09:03 -0700

    add console.log to main.js line14

--- src/exchange.js
+++ src/exchange.js
@@ -1,5 +1,14 @@
 export default class CurrencyExchange {
   static getCurrency(currency) {
     return fetch(`GET https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+    .then(function(response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return error;
+    })
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import CurrencyExchange from "./exchange.js"
+
+$(document).ready(function() {
+  $('#exchange-me').click(function() {
+    let currency = $('#currency').val();
+    let amountToExchange = $('#dollars').val();
+    // clear field function
+    CurrencyExchange.getCurrency(currency);
+      .then(function(response) {
+        console.log(response);
+        
+      })
+  })
+})
